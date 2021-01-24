d28eeeab080bad6eea485986c16833e14e0716de
maheshunhale <42297257+maheshunhale@users.noreply.github.com>
2021-01-24 17:25:28 +0530

    Update CollectorsInJava8_ConvertingIntoListSetMap.java

--- src/com/java8/methods/CollectorsInJava8_ConvertingIntoListSetMap.java
+++ src/com/java8/methods/CollectorsInJava8_ConvertingIntoListSetMap.java
@@ -12,7 +12,6 @@ import com.model.Product;
 public class CollectorsInJava8_ConvertingIntoListSetMap {
 
 	public static void main(String[] args) {
-		
 		List<Product> products = new ArrayList<>();
 		products.add(new Product("p1", "Mobile", 1000, 1, "Manufacturer 1"));
 		products.add(new Product("p2", "Laptop", 1000, 2, "Manufacturer 1"));
@@ -35,16 +34,13 @@ public class CollectorsInJava8_ConvertingIntoListSetMap {
 		Set<String> setOfProducts = products.stream().limit(3).map(Product::getId).collect(Collectors.toSet());
 		System.out.println("-------CONVERTED INTO SET------");
 		System.out.println(setOfProducts);
-		
-		
-		
+				
 		//Converting to Map
 		Map<String,Product> productMap = products.stream().collect(Collectors.toMap( p -> p.getId(), p -> p));
 		System.out.println("-------CONVERTED INTO MAP------");
 		for(String key:productMap.keySet()) {
 			System.out.println(productMap.get(key));
 		} 
-		
 		
 		//Getting comma separated product names from Product List
 		String productNames = products.stream().map(Product::getName).collect(Collectors.joining(", "));
@@ -57,8 +53,7 @@ public class CollectorsInJava8_ConvertingIntoListSetMap {
 		for(Long key:priceCounts.keySet()) {
 			System.out.println(key+":"+priceCounts.get(key));
 		}
-		
-		
+			
 		//Group by product price
 		Map<Long,List<Product>> productByPrice = products.stream().collect(Collectors.groupingBy(p -> p.getPrice()));
 		System.out.println("-------GROUPING BY PRICE------");
@@ -68,24 +63,18 @@ public class CollectorsInJava8_ConvertingIntoListSetMap {
 				System.out.println(p);
 			}
 		}
-		
-		
+				
 		//Highest Priced Product
 		System.out.println("-------HIGHEST PRICE PRODUCT------");
 		List<Product> highestPriceProduct = products.stream().sorted(Comparator.comparingLong(Product::getPrice).reversed())
 				.limit(1).collect(Collectors.toList());
 		System.out.println(highestPriceProduct);
-		
-		
-		
-		
+				
 		
 		//Distinct Sorted Three Highest priced products - ForEach
 		products.stream().sorted(Comparator.comparingLong(Product::getPrice).reversed())
 		.distinct().limit(3).map(Product::getId).forEach(System.out::println);
-		
-		
-		
+			
 		
 
 	}
